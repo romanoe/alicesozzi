d35d17008ef5bcc3903a5c945502ad8665c3b553
romanoe <noemi.romano@protonmail.com>
2021-01-26 00:45:49 +0100

    nuxt + vuetify

---
+++ components/Header.vue
@@ -0,0 +1,48 @@
+<!-- Style -->
+<style>
+/* .ant-layout-header {
+  background-color: white;
+} */
+
+#site_name {
+  font-weight: bold;
+  color : #C7287D;
+}
+
+
+#subtitle {
+  color : #C7287D;
+}
+
+
+</style>
+
+
+<!-- Template -->
+<template>
+<div>
+
+
+  <!-- <a-row type="flex" justify="center">
+    <a-col :span="6"> -->
+      Ciao
+      <!-- <img src="/assets/img/AliceSozzi.png"> -->
+<!--
+    </a-col>
+    <a-col :span="6"> -->
+      <!-- <p ><span id = "site_name">CABINET DE SOINS ALICE SOZZI</span></p> -->
+      <!-- <p ><span id = "subtitle" >PSYCHOLOGUE ET FASCIATHÉRAPEUTE, AGRÉE ASCA, RME</span></p> -->
+    <!-- </a-col> -->
+
+  <!-- </a-row> -->
+
+
+
+</div>
+</template>
+
+
+<!-- Script -->
+<script>
+export default {};
+</script>
---
+++ components/Menu.vue
@@ -0,0 +1,46 @@
+
+<!-- Style -->
+<style>
+.menu {
+  text-align: center;
+  font-weight: bold;
+}
+
+/* .ant-menu {
+    background-color: #E7F8F5;
+
+}
+
+.ant-menu-horizontal > .ant-menu-item > a {
+      margin : 0 60px 0 60px;
+      color:  #C7287D;
+} */
+</style>
+
+
+<!-- Template -->
+<template>
+<div class='menu'>
+
+
+      <NuxtLink to="/traitements">LES TRAITEMENTS</NuxtLink>
+
+
+      <NuxtLink to="/fascia">QU'EST-CE QU'UN FASCIA ?</NuxtLink>
+
+
+      <NuxtLink to="/mon-parcours">MON PARCOURS</NuxtLink>
+
+
+      <NuxtLink to="/info-tarifs">INFORMATIONS ET TARIFS</NuxtLink>
+
+
+      <NuxtLink to="/contact">CONTACT</NuxtLink>
+
+</div>
+</template>
+
+<!-- Script -->
+<script>
+export default {};
+</script>
