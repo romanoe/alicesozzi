5bb4d4178c1a38ddb1418e6b90b5bf3a185d54a7
romanoe <noemi.romano@protonmail.com>
2021-01-26 00:41:28 +0100

    nuxt.js + Vuetify set up

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+
+<template>
+<div>
+
+      <!-- Header -->
+       <Header/>
+
+
+
+       <!-- Menu -->
+        <Menu />
+
+
+
+     <!-- Footer -->
+  
+
+
+</div>
+</template>
+
+<script>
+export default {}
+</script>
