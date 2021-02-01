7ef6f538ab610612fdfa9505c685c9223a8e824e
romanoe <noemi.romano@protonmail.com>
2021-02-01 20:57:42 +0100

    dependencies device conflict

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,10 +48,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/vuetify',
-    ['@nuxtjs/device'],
-    device: {
-      defaultUserAgent: 'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36'
-    }
+    '@nuxtjs/device',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
