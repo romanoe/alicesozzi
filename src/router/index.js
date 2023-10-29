a59f0c414254c53d82e20b7faa1f553e485981ab
romanoe <noemi.romano@protonmail.com>
2023-10-29 18:23:54 +0100

    meta information

--- src/router/index.js
+++ src/router/index.js
@@ -7,37 +7,82 @@ import MonParcours from "@/views/MonParcours.vue";
 
 const routes = [
   {
-        path: '/public',
-        component: () => import('@/layouts/Default.vue'),
-    
-        path: '/',
-        name: 'ACCUEIL',
-        component: Home,
+    path: '/public',
+    component: () => import('@/layouts/Default.vue'),
+
+    path: '/',
+    name: 'ACCUEIL',
+    component: Home,
+    meta: {
+      title: 'Alice Sozzi - Acceuil',
+      metaTags: [
+        {
+          name: 'description',
+          content: "Page d'accueil de Alice Sozzi, fasciathérapeute à Lausanne"
+        }
+      ]
+    }
 
   },
   {
     path: '/traitements',
     name: 'LES TRAITEMENTS',
-    component: Traitements
+    component: Traitements,
+    meta: {
+      title: 'Les traitements de fasciathérapie',
+      metaTags: [
+        {
+          name: 'description',
+          content: "Les différents traitements de fasciathérapie fournie par Alice Sozzi, fasciathérapeute à Lausanne "
+        }
+      ]
+    }
   },
-      {
-        path: '/fascia',
-        name: "QU'EST-CE QU'UN FASCIA",
-        component: Fascia
-      },
-    
-      {
-        path: '/mon-parcours',
-        name: 'MON PARCOURS',
-        component: MonParcours
-      },
-      {
-        path: '/info-tarifs',
-        name: 'INFORMATIONS ET TARIFS',
-        component: InfoTarifs
-      }
+  {
+    path: '/fascia',
+    name: "QU'EST-CE QU'UN FASCIA",
+    component: Fascia,
+    meta: {
+      title: "Qu'est-ce qu'un fascia ? Quel est son domaine d'application ?",
+      metaTags: [
+        {
+          name: 'description',
+          content: "Description et définition d'un fascia ainsi que le domaines d'applications de la fasciathérapie menée par Alice Sozzi, fasciathérapeute à Lausanne"
+        }
+      ]
+    }
+  },
 
-    ]
+  {
+    path: '/mon-parcours',
+    name: 'MON PARCOURS',
+    component: MonParcours,
+    meta: {
+      title: "Le parcours professionnel d'Alice Sozzi, fasciathérapeute à LAusanne",
+      metaTags: [
+        {
+          name: 'description',
+          content: "Description des différentes étapes professionnelles d'Alice Sozzi, fasciathérapeute à Lausanne"
+        }
+      ]
+    }
+  },
+  {
+    path: '/info-tarifs',
+    name: 'INFORMATIONS ET TARIFS',
+    component: InfoTarifs,
+    meta: {
+      title: "Informations et tarifs",
+      metaTags: [
+        {
+          name: 'description',
+          content: "Informations, tarifs et prix pour une séance de fasciathérapie menée par Alice Sozzi, fasciathérapeute à Lausanne"
+        }
+      ]
+    }
+  }
+
+]
   ;
 
 const router = createRouter({
